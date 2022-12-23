83f5b13375d1c58cc1d4f5d00f53aa698aa805e2
BaptisteDlp <baptiste.delaporte@protonmail.com>
2022-12-23 11:06:09 +1100

    write invalid test that for getStorms plotStorms stormBehaviour

--- tests/testthat/test-getStorms.R
+++ tests/testthat/test-getStorms.R
@@ -3,65 +3,59 @@
 
 test_that("Tests invalid inputs", {
 
+
   #Checking `time_period`  validity
-  expect_error(getStorms(time_period = "hui"),
-               "time_period must be numeric")
+  expect_error(getStorms(basin = "BA"))
+  expect_error(getStorms(basin = c("SA","SP")))
 
-  expect_error(getStorms(time_period = 2015.6, name = "PAM"),
-               "time_period must be as integers")
-
-  expect_error(getStorms(time_period = c(1850,2020)),
-               "lower bound of time range is not valid")
-
-  expect_error(getStorms(time_period = c(2000,2030)),
-               "upper bound of time range is not valid")
-
-
-  expect_error(getStorms(time_period =  c(2000,2005,2020)),
-               "time_period must be either length 1 or 2")
+  #Checking `time_period`  validity
+  expect_error(getStorms(time_period = "hui"))
+  expect_error(getStorms(time_period = 2015.6, name = "PAM"))
+  expect_error(getStorms(time_period = c(1850,2020)))
+  expect_error(getStorms(time_period = c(2000,2030)))
+  expect_error(getStorms(time_period =  c(2000,2005,2020)))
 
   #Checking `name` validity
-  expect_error(getStorms(time_period = 2010, name = 1),
-               "name must be a vector of characters")
-
+  expect_error(getStorms(time_period = 2010, name = 1))
 
   #Checking `time_period` and `name` matching
-  expect_error(getStorms(time_period = c(2016,2015), name = "WINSTON"),
-               "name and time_period must be the same length")
+  expect_error(getStorms(time_period = c(2016,2015), name = "WINSTON"))
 
   #Checking `loi` validity
-  # expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-  #                        loi = "hui"),
-  #              "invalid entry for loi")
-  #
-  # expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-  #                        loi = 165),
-  #              "invalid class for loi")
-  #
-  # expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-  #                        loi = c("hj","jk")),
-  #              "loi must be length one ")
-  #
-  # expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-  #                        loi = c(380, 100)),
-  #              "loi must have valid lon/lat coordinates ")
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = "hui"))
+
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = 165))
+
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = c("hj","jk")))
+
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = c(380, 100)))
+
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = "Nouvelle Caledonie"))
+
+  expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
+                         loi = c("Vanuatu", "Fiji")))
+
 
 
   #Checking `max_dist` validity
   expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-                         loi = c(165,-17), max_dist = "hui"),
-               "max_dist must be numeric ")
-
+                         loi = c(165,-17), max_dist = "hui"))
   expect_error(getStorms(time_period = c(2016,2015), name = c("WINSTON","PAM"),
-                         loi = c(165,-17), max_dist = c(1,3)),
-               "max_dist must be a length 1 vector ")
+                         loi = c(165,-17), max_dist = c(1,3)))
 
 
 
 
   #Checking access to data
-  # expect_error(getStorms(time_period = 2017, name = "PAM"),
-  #               "Storm not found")
+  expect_error(getStorms(time_period = 2015, name = "PM"),
+               "Storm not found")
+  expect_error(getStorms(time_period = 2017, name = "PAM"),
+                "Storm not found")
 
 
 
