ba7edc7f2bb89c49311ae4925eac7018e743eb80
BaptisteDlp <baptiste.delaporte@protonmail.com>
2023-02-15 14:56:06 +1100

    Upgrade tests
    Former-commit-id: 0c529b4fb2d26532bf13aad49361a95945a24298

--- tests/testthat/test-writeRast.R
+++ tests/testthat/test-writeRast.R
@@ -1,27 +1,54 @@
 
 
 
-test_that("Tests invalid inputs", {
 
+test_that("Tests checkInputsWr function", {
+
+  msw <- stormBehaviour_sp(pam, verbose = 0)
 
   msw <- terra::rast(system.file("extdata", "PAM_MSW.tiff", package = "StormR"))
 
   #Checking rast validity
-  expect_error(writeRast())
+  expect_error(writeRast(format = ".tiff",
+                         filename = NULL,
+                         path = "./"))
 
   #Checking format validity
-  expect_error(writeRast(msw,format = 1))
-  expect_error(writeRast(msw,format = ".pdf"))
-  expect_error(writeRast(msw,format = c(".tiff",".nc")))
+  expect_error(writeRast(rast = msw,
+                         format = 1,
+                         filename = NULL,
+                         path = "./"))
+
+  expect_error(writeRast(rast = msw,
+                         format = ".pdf",
+                         filename = NULL,
+                         path = "./"))
+
+  expect_error(writeRast(rast = msw,
+                         format = c(".tiff",".nc"),
+                         filename = NULL,
+                         path = "./"))
 
   #Checking filename validity
-  expect_error(writeRast(sts, filename = T))
-  expect_error(writeRast(sts, filename = 2))
+  expect_error(writeRast(rast = msw,
+                         format = ".tiff",
+                         filename = TRUE,
+                         path = "./"))
+
+  expect_error(writeRast(rast = msw,
+                         format = ".tiff",
+                         filename = 2,
+                         path = "./"))
 
   #Checking path validity
-  expect_error(writeRast(sts, path = T))
-  expect_error(writeRast(sts, path = 2))
+  expect_error(writeRast(rast = msw,
+                         format = ".tiff",
+                         filename = NULL,
+                         path = TRUE))
 
-
+  expect_error(writeRast(rast = msw,
+                         format = ".tiff",
+                         filename = NULL,
+                         path = 2))
 
 })
