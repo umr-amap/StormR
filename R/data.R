5665f2d3c1c8e02f824967b18aa4d7ee9f246ff0
BaptisteDlp <baptiste.delaporte@protonmail.com>
2023-03-23 11:11:33 +1100

    Remove pam, sts_nc and test_database.rda
    Former-commit-id: b421e61f0585979120413bab02d62837909fe0d7

--- R/data.R
+++ R/data.R
@@ -1,22 +1,5 @@
-#' PAM tropical cyclone
-#'
-#' Gathers informations about the category 5 TC PAM (2015) over Vanuatu with a
-#'  buffer of 300km
-#'
-#' @format ## `pam`
-#' a StormsList object generated with Storms
-"pam"
 
 
-
-#' ERICA and NIRAN tropical cyclones
-#'
-#' Gathers informations about both the category 4 and 5 TC ERICA (2003) and
-#' NIRAN (2021) over New Caledonia with a buffer of 300km
-#'
-#' @format ## `sts_nc`
-#' a StormsList object generated with Storms
-"sts_nc"
 
 
 
@@ -29,24 +12,3 @@
 "eezNC"
 
 
-#' example of dataset
-#'
-#' Toy dataset extracted from the IBTrACS.SP.v04r00.nc file. Provides all the
-#' tropical cyclones that occured around Vanuatu from 2015 to 2016 and around
-#' New Caledonia from 2020 to 2021.
-#' References:
-#' * Knapp, K. R., M. C. Kruk, D. H.
-#' Levinson, H. J. Diamond, and C. J. Neumann, 2010: The International Best
-#' Track Archive for Climate Stewardship (IBTrACS): Unifying tropical cyclone
-#' best track data. Bulletin of the American Meteorological Society, 91,
-#' 363-376. doi:10.1175/2009BAMS2755.1
-#' 
-#' * Knapp, K. R., H. J. Diamond, J. P. Kossin, M. C. Kruk, C. J. Schreck,
-#' 2018: International Best Track Archive for Climate Stewardship (IBTrACS)
-#' Project, Version 4. NOAA National Centers for Environmental Information.
-#' doi:10.25921/82ty-9e16
-#' 
-#' 
-#' @format ## `test_dataset`
-#' a StormsDataset object
-"test_dataset"
