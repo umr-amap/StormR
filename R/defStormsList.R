d55419fa0f324b9ce85e8708d77b254a6cfd043d
BaptisteDlp <baptiste.delaporte@protonmail.com>
2023-12-12 13:33:25 +1100

    minor fix

--- R/defStormsList.R
+++ R/defStormsList.R
@@ -1068,7 +1068,7 @@ defStormsList <- function(sds,
   
   if(is.null(names(scalePalette))){
     # If scalePalette has no names, provide default ones
-    names(scalePalette) <- seq(1, length(scalePalette))
+    names(scalePalette) <- seq(0, length(scalePalette))
     
   }
   
